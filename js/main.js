bf6133468fcf82a616b91729bd4e9978fc48c170
lehvolk <lehvolk@gmail.com>
2015-02-14 12:29:36 +0300

    removing custom bootstrap css news tuning and internationalization

--- js/main.js
+++ js/main.js
@@ -9,6 +9,137 @@ var ml = function (ru, eng) {
     };
 };
 
+var newsItem = function (date, header, text) {
+    this.date = date;
+    this.header = header;
+    this.text = text;
+};
+
+var news = [
+    new newsItem(
+        '04.04.2013',
+        new ml('Трудоустройство', 'Job Position'),
+        new ml(
+            'Международная инвестиционная компания <a href="http://www.worldquant.com "> WorldQuant</a>,' +
+            'имеющая филиалы в крупнейших городах мира, открывает набор сотрудников, примерно 10 человек. В России у' +
+            'компании' +
+            'представлено подразделение research, которое размещается в двух офисах, в Москве и Санкт-Петербурге.' +
+            'В качестве претендентов на открытые вакансии компанию интересуют выпускники мат-меха, а также студенты 4-5' +
+            'курсов на перспективу, в основном по специальности теория вероятностей и мат. статистика' +
+            'для построения, анализа и предсказания финансовых рынков.' +
+            '<br/>По всем интересующим вопросам обращаться на нашу кафедру (аудитории 4512, 4515).'
+            ,
+            'International investment company <a href="http://www.worldquant.com "> WorldQuant</a>,' +
+            'open about 10 job positions. In Russia company has research department in Moscow and Saint-Petersburg.' +
+            'Candidate should has mathematic degree (preferred Mathemtics and Mechanics departments)' +
+            ' or be a students of last courses, degree of statistics and probability theory is a strong plus' +
+            '<br/>Contact with statistic and probability deparment for more information (lectures 4512, 4515).'
+        )
+    ),
+    new newsItem(
+        '24.08.2013',
+        new ml('Трудоустройство', 'Job Position'),
+        new ml(
+            'Компания ООО "ГАЗПРОМ ЭКСПОРТ" ищет сотрудников в отдел Управления по рискам, окончивших кафедру' +
+            'Теории' +
+            'вероятностей и математической статистики.' +
+            '<br/> <a href="files/Gazprom-export.pdf">Подробнее...</a>'
+            ,
+            '"GazProm Export" open position in Risk Management department,' +
+            '<br/> <a href="files/Gazprom-export.pdf">more information...</a>'
+        )
+    ),
+    new newsItem(
+        '28.09.2013',
+        new ml('Объявление о конференции', 'Conference announcement'),
+        new ml(
+            '<a href=" http://taac.org.ua">III International Scientific Conference of Students and Young Scientists' +
+            '"Theoretical and Applied Aspects of Cybernetics" (TAAC-2013)</a>' +
+            '<br/>Киев, 25 - 29 ноября 2013 г.' +
+            '<br/>Окончание регистрации и предоставления докладов: 20 октября 2013 г.' +
+            '<br/><a href="files/taac2013_en.pdf">Подробнее...</a>'
+            ,
+            '<a href=" http://taac.org.ua">III International Scientific Conference of Students and Young Scientists' +
+            '"Theoretical and Applied Aspects of Cybernetics" (TAAC-2013)</a>' +
+            '<br/>Kiev, 25 - 29 november 2013' +
+            '<br/>Registration end: 20 october 2013' +
+            '<br/><a href="files/taac2013_en.pdf">more information...</a>'
+        )
+    ),
+    new newsItem(
+        '04.10.2013',
+        new ml('Объявление о конференции', 'Conference announcement'),
+        new ml(
+            '<a href=" http://www.gpsd-ulm2014.de/">11th German Probability and Statistics Days</a>' +
+            '<br/>Ульм, Германия, 4 - 7 марта 2014 г.' +
+            ' <br/>Окончание регистрации: 15 января 2014 г., предоставить доклады необходимо до 30 ноября 2013 г.' +
+            '<br/>Параллельно пройдет другая конференция <a href=" http://graduiertenkolleg.gpsd-ulm2014.de/">Conference' +
+            'on' +
+            'Modeling, Analysis and Simulation in Economathematics</a>' +
+            '<br/>Ульм, Германия, 6 - 8 марта 2014 г.' +
+            '<br/>Дополнительной регистрации не требуется.'
+            ,
+            '<a href=" http://www.gpsd-ulm2014.de/">11th German Probability and Statistics Days</a>' +
+            '<br/>Ulm, Germany, 4 - 7 march 2014' +
+            ' <br/>Registration end: 15 january 2014 г., submit presentations 30 november 2013' +
+            '<br/>Another conference <a href=" http://graduiertenkolleg.gpsd-ulm2014.de/">Conference' +
+            'on' +
+            'Modeling, Analysis and Simulation in Economathematics</a>' +
+            '<br/>Ulm, Germany, 6 - 8 march 2014' +
+            '<br/>No additional registration.'
+        )
+    ),
+    new newsItem(
+        '07.10.2013',
+        new ml('Объявление о конференции', 'Conference announcement'),
+        new ml(
+            '<a href=" http://www.ipiran.ru/conference/stabil2014/">XXXII International Seminar on Stability' +
+            'Problems' +
+            'for Stochastic Models</a>' +
+            '<br/>Тронхейм, Норвегия, 16 - 21 июня 2014 г.' +
+            '<br/>Предоставить доклады необходимо до 1 марта 2014 г.'
+            ,
+            '<a href=" http://www.ipiran.ru/conference/stabil2014/">XXXII International Seminar on Stability' +
+            'Problems' +
+            'for Stochastic Models</a>' +
+            '<br/>Trondheim, Norway, 16 - 21 june 2014' +
+            '<br/>submit papers till 1 march 2014'
+        )
+    ),
+    new newsItem(
+        '09.10.2013',
+        new ml('Объявление о конференции', 'Conference announcement'),
+        new ml('<a href=" http://www.vilniusconference2014.mif.vu.lt/">11th International Vilnius Conference on' +
+            'Probability' +
+            'and Mathematical Statistics</a>' +
+            '<br/>Вильнюс, Литва, 30 июня - 4 июля 2014 г.' +
+            '<br/>Окончание ранней регистрации: 1 мая 2014 г.'
+            ,
+            '<a href=" http://www.vilniusconference2014.mif.vu.lt/">11th International Vilnius Conference on' +
+            'Probability' +
+            'and Mathematical Statistics</a>' +
+            '<br/>Vilnius, Litva, 30 june - 4 july 2014 г.' +
+            '<br/>End of registration: 1 may 2014 г.'
+        )
+    ),
+    new newsItem(
+        '09.10.2013',
+        new ml('Объявление о конференции', 'Conference announcement'),
+        new ml('<a href=" http://www.vilniusconference2014.mif.vu.lt/">11th International Vilnius Conference on' +
+            'Probability' +
+            'and Mathematical Statistics</a>' +
+            '<br/>Вильнюс, Литва, 30 июня - 4 июля 2014 г.' +
+            '<br/>Окончание ранней регистрации: 1 мая 2014 г.',
+            '<a href=" http://www.vilniusconference2014.mif.vu.lt/">11th International Vilnius Conference on' +
+            'Probability' +
+            'and Mathematical Statistics</a>' +
+            '<br/>Vilnius, Litva, 30 june - 4 july 2014 г.' +
+            '<br/>End of registration: 1 may 2014 г.'
+        )
+    )
+
+];
+
 var staff = {
     data: [
         {
@@ -218,8 +349,8 @@ kafedra.controller('MenuController', function ($scope, $location) {
     };
 }).controller('StaffController', function ($scope, $timeout) {
     $scope.staff = staff.data;
-    $scope.itemLink = function(staffItem){
-      return staff.link(staffItem);
+    $scope.itemLink = function (staffItem) {
+        return staff.link(staffItem);
     };
     $timeout(function () {
         $('.slick-track').slick({
@@ -240,7 +371,14 @@ kafedra.controller('MenuController', function ($scope, $location) {
         $translate.use(langKey);
         $route.reload();
     };
-});;
+}).controller('NewsController', function ($scope, $sce) {
+    $scope.topNews = news.reverse().slice(1, 10);
+    $scope.allNews = news.reverse();
+    $scope.showNews = function(item){
+        return $sce.trustAsHtml(item.text.view());
+    }
+
+});
 
 kafedra.directive('scrollOnClick', function () {
     return {
